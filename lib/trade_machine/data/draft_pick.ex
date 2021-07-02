5b9caed2ef21a7b2900b97927679b8d722abc914
Akosua <aaasante@uwaterloo.ca>
2021-07-02 16:35:12 -0400

    Ecto Setup [2]: Figured out how to add related models

--- lib/trade_machine/data/draft_pick.ex
+++ lib/trade_machine/data/draft_pick.ex
@@ -1,11 +1,17 @@
 defmodule TradeMachine.Data.DraftPick do
   use TradeMachine.Schema
 
+  alias TradeMachine.Data.Team
+
   schema "draft_pick" do
-    field :round, :decimal # TODO: Maybe decimal? Maybe eventually only allow integers?
-    field :pick_number, :integer
     field :season, :integer
     field :type, Ecto.Enum, values: [majors: "1", high: "2", low: "3"]
+    field :round, :decimal # TODO: Maybe decimal? Maybe eventually only allow integers?
+    field :pick_number, :integer
+
+    belongs_to :held_by, Team, source: :currentOwnerId, foreign_key: :currentOwnerId
+    belongs_to :original_owner, Team, source: :originalOwnerId, foreign_key: :originalOwnerId
+
     timestamps()
   end
 
---
+++ lib/trade_machine/data/team.ex
@@ -0,0 +1,22 @@
+defmodule TradeMachine.Data.Team do
+  use TradeMachine.Schema
+
+  alias TradeMachine.Data.DraftPick
+
+  schema "team" do
+    field :name, :string
+    field :espn_id, :integer
+    field :status, Ecto.Enum, values: [active: "1", disabled: "2"]
+    field :espn_team, :map, load_in_query: false
+
+    has_many :held_picks, DraftPick, foreign_key: :currentOwnerId
+    has_many :original_picks, DraftPick, foreign_key: :originalOwnerId
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params \\ %{}) do
+    struct
+    |> cast(params, [])
+  end
+end
