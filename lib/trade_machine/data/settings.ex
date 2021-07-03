640a1cac6f8a1eec77678b71bfa5d226f7999ee2
Akosua <aaasante@uwaterloo.ca>
2021-07-02 20:19:44 -0400

    Ecto Setup [7]: Settings

---
+++ lib/trade_machine/data/settings.ex
@@ -0,0 +1,20 @@
+defmodule TradeMachine.Data.Settings do
+  use TradeMachine.Schema
+
+  alias TradeMachine.Data.User
+
+  schema "settings" do
+    field :trade_window_start, :time
+    field :trade_window_end, :time
+    field :downtime, :map
+
+    belongs_to :modified_by, User
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params \\ %{}) do
+    struct
+    |> cast(params, [])
+  end
+end
