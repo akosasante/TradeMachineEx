92810856b02d658520d984bc6afa666c87950f29
Akosua <aaasante@uwaterloo.ca>
2025-09-27 17:47:02 -0400

    replace oban migration to regenerate tables needed for dev mode

--- priv/repo/migrations/20250927203214_add_oban_jobs_table.exs
+++ priv/repo/migrations/20250927203214_add_oban_jobs_table.exs
@@ -1,13 +1,13 @@
-defmodule MyApp.Repo.Migrations.AddObanJobsTable do
+defmodule TradeMachine.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
   def up do
-    Oban.Migrations.up(Application.fetch_env!(:trade_machine, Oban))
+    Oban.Migration.up(version: 12)
   end
 
   # We specify `version: 1` in `down`, ensuring that we'll roll all the way back down if
   # necessary, regardless of which version we've migrated `up` to.
   def down do
-    Oban.Migrations.down(version: 1)
+    Oban.Migration.down(version: 1)
   end
 end
