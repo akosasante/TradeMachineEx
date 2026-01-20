c39e0ffc7946d6c67be9e4842ed84f792fffbea9
Akosua <aaasante@uwaterloo.ca>
2026-01-20 12:12:59 -0500

    lint

--- test/support/test_helper.ex
+++ test/support/test_helper.ex
@@ -5,7 +5,7 @@ defmodule TestHelper do
 
   @doc """
   Sets the search_path to 'test' schema for a sandboxed database connection.
-  
+
   This is necessary because the test environment uses the 'test' schema
   to isolate test data from production and staging schemas.
   """
