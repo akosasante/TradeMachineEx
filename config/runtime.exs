669993777e6945dff2f20d6950931dde5a2a4131
Akosua <aaasante@uwaterloo.ca>
2026-01-20 21:30:22 -0500

    chore: update ESPN sync schedule to 9:40 PM EST for testing
    - Change cron from 15 2 * * * (9:15 PM EST) to 40 2 * * * (9:40 PM EST)
    - 9:40 PM EST = 02:40 UTC (next day)

--- config/runtime.exs
+++ config/runtime.exs
@@ -116,7 +116,7 @@ if config_env() != :test do
         {Oban.Plugins.Cron,
          crontab: [
            {"0 2 * * *", TradeMachine.Jobs.MinorsSync},
-           {"15 2 * * *", TradeMachine.Jobs.EspnTeamSync}
+           {"40 2 * * *", TradeMachine.Jobs.EspnTeamSync}
          ]}
       ]
     else
