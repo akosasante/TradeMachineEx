204527508001023dd1a73f8887d6478561ee4874
Akosua <aaasante@uwaterloo.ca>
2026-01-20 19:45:53 -0500

    fix: use Ecto's prefix() function in Oban migration
    - Use prefix() which returns the migration_default_prefix from repo config
    - Removes broken repo_config helper that was trying to infer repo name
    - This ensures Oban tables are created in the correct schema

--- priv/repo/migrations/20250927203214_add_oban_jobs_table.exs
+++ priv/repo/migrations/20250927203214_add_oban_jobs_table.exs
@@ -2,25 +2,16 @@ defmodule TradeMachine.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
   def up do
-    # Get the prefix from the repo's migration_default_prefix config
-    prefix = repo_config(:migration_default_prefix) || "public"
-    Oban.Migration.up(version: 12, prefix: prefix)
+    # Use Ecto's prefix() function which returns the migration prefix
+    # This is set by migration_default_prefix in the repo config
+    migration_prefix = prefix() || "public"
+    Oban.Migration.up(version: 12, prefix: migration_prefix)
   end
 
   # We specify `version: 1` in `down`, ensuring that we'll roll all the way back down if
   # necessary, regardless of which version we've migrated `up` to.
   def down do
-    prefix = repo_config(:migration_default_prefix) || "public"
-    Oban.Migration.down(version: 1, prefix: prefix)
-  end
-
-  defp repo_config(key) do
-    repo =
-      __MODULE__
-      |> Module.split()
-      |> Enum.take(2)
-      |> Module.concat()
-
-    Application.get_env(:trade_machine, repo)[key]
+    migration_prefix = prefix() || "public"
+    Oban.Migration.down(version: 1, prefix: migration_prefix)
   end
 end
