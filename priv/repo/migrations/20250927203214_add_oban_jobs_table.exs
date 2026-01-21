769e01e58a646d7385e632f933e17620a8bdc2a6
Akosua <aaasante@uwaterloo.ca>
2026-01-20 19:14:53 -0500

    fix: use correct schema prefix in Oban migration
    - Pass prefix parameter to Oban.Migration.up/down
    - Dynamically read prefix from repo's migration_default_prefix config
    - Ensures oban_peers table is created in correct schema (staging vs public)

--- priv/repo/migrations/20250927203214_add_oban_jobs_table.exs
+++ priv/repo/migrations/20250927203214_add_oban_jobs_table.exs
@@ -2,12 +2,24 @@ defmodule TradeMachine.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
   def up do
-    Oban.Migration.up(version: 12)
+    # Get the prefix from the repo's migration_default_prefix config
+    prefix = repo_config(:migration_default_prefix) || "public"
+    Oban.Migration.up(version: 12, prefix: prefix)
   end
 
   # We specify `version: 1` in `down`, ensuring that we'll roll all the way back down if
   # necessary, regardless of which version we've migrated `up` to.
   def down do
-    Oban.Migration.down(version: 1)
+    prefix = repo_config(:migration_default_prefix) || "public"
+    Oban.Migration.down(version: 1, prefix: prefix)
+  end
+
+  defp repo_config(key) do
+    repo = __MODULE__
+    |> Module.split()
+    |> Enum.take(2)
+    |> Module.concat()
+    
+    Application.get_env(:trade_machine, repo)[key]
   end
 end
